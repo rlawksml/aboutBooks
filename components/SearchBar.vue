49117777984c98c58b1df4f6f5f43c13042de1dc
JINHEE KIM <rlawksml@gmail.com>
2025-02-19 00:17:33 +0900

    kakao pai 연동, ofetch 적용 nuxt에서는 axios를 지원하지 않는다

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="flex justify-center my-10">
-        <input class="" type="text" placeholder="검색어를 입력하세요">
-        <button class="">검색</button>
-
+        <input v-model="searchData" class="" type="text" placeholder="검색어를 입력하세요">
+        <button @click="searchClick" class="">검색</button>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup >
+    import {ref} from 'vue'
+    import useBooksStore from "../stores/books"
 
+    let searchData = ref('')
+
+    const booksStore = useBooksStore()
+    
+    const searchClick = () => {        
+        booksStore.searchBook(searchData.value)
+    }
+    
 </script>
 
 <style lang="scss" scoped>
